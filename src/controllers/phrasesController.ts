bef96f64b2bd216112cc8f7d82dbb939045e66a3
GuilhermeUberti <guilherme.uberti@hotmail.com>
2022-05-16 11:14:26 -0300

    CRUD finalizado

--- src/controllers/phrasesController.ts
+++ src/controllers/phrasesController.ts
@@ -3,9 +3,47 @@ import { Phrase } from '../models/Phrase';
 
 export const createPhrases = async (req: Request, res: Response) => {
     let { author, publishing } = req.body;
-
     let newPhrase = await Phrase.create({ author, publishing });
 
+    res.status(201);
     res.json({ id: newPhrase.id, author, publishing });
 }
 
+export const listPhrases = async (req: Request, res: Response) => {
+    let list = await Phrase.findAll();
+
+    res.json({ list })
+}
+
+export const listOnePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    let phrase = await Phrase.findByPk(id);
+    if (phrase) {
+        res.json({ phrase })
+    } else {
+        res.json({ error: "Frase não encontrada!" });
+    }
+}
+
+export const updatePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    let { author, publishing } = req.body;
+    let phrase = await Phrase.findByPk(id);
+
+    if (phrase) {
+        phrase.author = author;
+        phrase.publishing = publishing;
+        await phrase.save();
+
+        res.json({ phrase });
+    } else {
+        res.json({ error: "Frase inexistente!" });
+    }
+}
+
+export const deletePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    await Phrase.destroy({ where: { id } });
+
+    res.json({});
+}
--- src/routes/phrasesRoute.ts
+++ src/routes/phrasesRoute.ts
@@ -4,6 +4,10 @@ import * as PhrasesController from '../controllers/phrasesController';
 const router = Router();
 
 router.post('/frases', PhrasesController.createPhrases);
+router.get('/frases', PhrasesController.listPhrases);
+router.get('/frase/:id', PhrasesController.listOnePhrase);
+router.put('/frase/:id', PhrasesController.updatePhrase);
+router.delete('/frase/:id', PhrasesController.deletePhrase);
 
 export default router;
 
